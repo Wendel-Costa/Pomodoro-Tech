05d4098b0916d20b6b45da7380a5a7354c1ec2ec
Wendel Davi <149116208+Wendel-Costa@users.noreply.github.com>
2025-09-20 00:12:08 +0000

    creating content and container

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import './styles/theme.css';
 import './styles/global.css';
 
-import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
-
 export function App() {
   return (
     <>
-      <Heading>
-        oi
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-        placeat sed autem aspernatur itaque odio laboriosam fugit cumque
-        molestias enim fugiat sapiente reiciendis, vero cupiditate magni in
-        pariatur hic nihil!
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section></section>
+        </div>
+      </div>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,5 @@ import { App } from './App';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <h1>Olá mundo teste</h1>
   </StrictMode>,
 );
