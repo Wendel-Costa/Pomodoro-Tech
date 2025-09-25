b7f97a34ada0444a6f2fe24907e24d517587a76c
Wendel Davi <149116208+Wendel-Costa@users.noreply.github.com>
2025-09-25 00:40:59 +0000

    creating cycles component

---
+++ src/components/Cycles/index.tsx
@@ -0,0 +1,21 @@
+import styles from './styles.module.css';
+
+export function Cycles() {
+   return (
+      <div className={styles.cycle}>
+         <span>Ciclos</span>
+
+         <div className={styles.cycleDots}>
+            <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
+            <span
+               className={`${styles.cycleDot} ${styles.shortBreakTime}`}
+            ></span>
+            <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
+            <span
+               className={`${styles.cycleDot} ${styles.longBreakTime}`}
+            ></span>
+            <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
+         </div>
+      </div>
+   );
+}
