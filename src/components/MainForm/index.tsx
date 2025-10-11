2d48c312fb3ae72a89c6f9de7db81fe20b59de7c
Wendel Davi <149116208+Wendel-Costa@users.noreply.github.com>
2025-10-11 21:35:35 +0000

    creating pages and templates

---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,34 @@
+//import styles from 'styles.module.css';
+import { DefaultInput } from '../DefaultInput';
+import { Cycles } from '../Cycles';
+import { DefaultButton } from '../DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
+
+export function MainForm() {
+   return (
+      <form className='form' action=''>
+         <div className='formRow'>
+            <DefaultInput
+               id='meuInput'
+               type='text'
+               labelText='chama'
+               placeholder='Digite algo'
+            />
+         </div>
+
+         <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+         </div>
+
+         <div className='formRow'>
+            <Cycles />
+         </div>
+
+         <div className='formRow'>
+            <DefaultButton>
+               <PlayCircleIcon />
+            </DefaultButton>
+         </div>
+      </form>
+   );
+}
---
+++ src/templates/NotFound/index.tsx
@@ -0,0 +1,17 @@
+import { Container } from '../../components/Container';
+import { MainTemplate } from '../../templates/MainTemplate';
+
+export function NotFound() {
+   return (
+      <MainTemplate>
+         <Container>
+            <p>
+               Lorem ipsum dolor sit amet consectetur adipisicing elit.
+               Voluptatem voluptas impedit reprehenderit. Ipsa ea incidunt
+               quisquam corporis ut nisi debitis saepe accusamus? Perspiciatis
+               dolorum eum voluptatum, quidem facilis cupiditate unde!
+            </p>
+         </Container>
+      </MainTemplate>
+   );
+}
